0b8b96b561833150b9331598db8bd2dd622ba581
sushil016 <sahanisushil325@gmail.com>
2026-04-14 11:25:42 +0530

    feat: add tech icon components for Docker, Kubernetes, Azure, GCP, Redis, Kafka, ArgoCD, Grafana, Helm

---
+++ components/technologies/Azure.tsx
@@ -0,0 +1,10 @@
+export default function Azure() {
+  return (
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fill="#0078D4"
+        d="M13.05 4.24l-6.552 16.22H0L6.09 8.433 13.05 4.24zm.718.63L24 20.46H6.83l9.363-1.804 3.43-8.318-5.855-5.463z"
+      />
+    </svg>
+  );
+}
